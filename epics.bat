0feab80a56ad6feefdd9d2934549dd0986b9a42d
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2024-02-05 09:35:09 +0300

    Added functions to build epics modules.

--- epics.bat
+++ epics.bat
@@ -13,8 +13,8 @@ set EPICS_BASE=%epics%\base
 set EPICS_HOST_ARCH=%epics_host%
 set support=%epics%\support
 
-if not exist %epics%   mkdir %epics%
-if not exist %support% mkdir %support%
+if not exist %epics%\   mkdir %epics%
+if not exist %support%\ mkdir %support%
 
 git --version >nul 2>&1 && (
 	for /f "delims=" %%a in ('where git') do echo found git @ %%a
@@ -31,7 +31,7 @@ make --version >nul 2>&1 && (
 )
 
 if exist "%perl_home%\perl\bin\perl.exe" (
-	echo found perl @ "%perl_home%\perl\bin\perl.exe".
+	echo found perl @ %perl_home%\perl\bin\perl.exe.
 ) else (
 	echo Strawberry Perl not found. Install it and try again.
 )
@@ -55,32 +55,66 @@ if not exist %epics%\base\ (
 
 cd %support%
 
-REM if not exist %epics%\support\seq\ (
-REM 	echo Building sequencer module
-REM 	xcopy /i /Y /E %nfs%\seq-2.2.9 %support%\seq >nul 2>&1
-REM 	cd %support%\seq
-REM 	xcopy /Y %release_files%\seq.RELEASE configure\RELEASE >nul 2>&1
-REM 	make >nul 2>&1
-REM ) else (
-REM 	echo Seq 2.2.9 is already installed.
-REM )
-
-call :build_module seq -2.2.9
+call :build_module seq,-2.2.9
 call :build_module autosave
+call :build_module ether-ip
+call :build_module sscan
+call :build_module calc
+call :build_module ipac
+call :build_module asyn
+call :build_module scaler
+call :build_module stream-device
+call :build_module modbus
+call :build_module busy
+call :build_module std
+call :build_module mca
+
+call :build_module area-detector-support
+call :build_module area-detector-core
 
 cd %USERPROFILE%
-set PATH=%_path%
+call :clear_env
 exit /B 0
+
+:clear_env
+
+set PATH=%_path%
+set nfs=
+set epics_host=
+set epics=
+set version=
+set visual_studio_home=
+set perl_home=
+set release_files=
+set support=
+set ad_base=
+set _path=
+
+EXIT /B 0
 
 :build_module
 
 if not exist %epics%\support\%~1\ (
 	echo Building %~1 module ...
-	xcopy /i /Y /E %nfs%\%~1%~2 %support%\%~1 >nul 2>&1
+	xcopy /i /Y /E %nfs%\%~1%~2 %support%\%~1
 	cd %support%\%~1
-	xcopy /Y %release_files%\%~1.RELEASE configure\RELEASE >nul 2>&1
-	make >nul 2>&1
+	xcopy /Y %release_files%\%~1.RELEASE configure\RELEASE
+	make
 ) else (
 	echo %~1 is already installed.
 )
+EXIT /B 0
+
+:build_ad_module
+
+if not exist %ad_base%\%~1\ (
+	echo Building %~1 module ...
+	xcopy /i /Y /E %nfs%\%~1 %ad_base%\%~1
+	cd %ad_base%\%~1
+	xcopy /Y %release_files%\%~1.RELEASE configure\RELEASE
+	make
+) else (
+	echo %~1 is already installed.
+)
+
 EXIT /B 0
